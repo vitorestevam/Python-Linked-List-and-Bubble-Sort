da0dcbc6a9f109adc86aea920f55d62dd90f8c4e
Vitor Estevam <vitorestevam02@gmail.com>
2020-10-13 18:13:06 -0300

    README-txt CREATED

---
+++ README.txt
@@ -0,0 +1,9 @@
+o order_pixels orderna os pixels de cada linha horizontal da imagem
+pelo seu BRILHO, indo do mais claro para o mais escuro.
+Separei 4 imagens que exemplificam bem o que o script
+faz, mas fique a vontade para testar com a imagem que quiser.
+
+para fazer implementei listas encadeadas e bubblesort.
+utilizei também as bibliotecas open cv e numpy
+
+usa em uma imagem de cada vez. Deixei comentado para facilitar
--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import cv2 as cv
 from imageControl import ImageControl 
 
-#o order_pixels rderna os pixels de cada linha horizontal da imagem
-#pelo seu BRILHO, indo do mais claro para o mais escuro.
-#Separei 4 imagens que exemplificam 
-#bem o que o script faz, mas fique a vontade para testar com a imagem que quiser.
-
-#para fazer implementei listas encadeadas e bubblesort.
-#utilizeitambém as bibliotecas open cv e numpy
-
 #usa em uma imagem de cada vez. Deixei comentado para facilitar
 
 img = ImageControl("images/image3.jpg")
